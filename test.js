49a95e93c59fe0c39645400873847f190f25f51e
Titus Wormer <tituswormer@gmail.com>
2018-08-20 12:24:40 +0200

    Move tests into `test.js`

---
+++ test.js
@@ -0,0 +1,39 @@
+'use strict'
+
+var test = require('tape')
+var u = require('unist-builder')
+var squeezeParagraphs = require('.')
+
+test(function(t) {
+  t.deepEqual(
+    squeezeParagraphs(
+      u('root', [
+        u('paragraph', []),
+        u('paragraph', [u('text', 'first')]),
+        u('paragraph', []),
+        u('paragraph', [u('text', ''), u('text', '  \n')]),
+        u('paragraph', [
+          u('text', 'second'),
+          u('text', ' '),
+          u('text', 'value')
+        ]),
+        u('paragraph', []),
+        u('list', {ordered: false, start: null, loose: false}, [
+          u('listItem', {loose: false}, [u('paragraph', [])]),
+          u('listItem', {loose: false}, [
+            u('paragraph', [u('text', ' '), u('text', ' ')])
+          ])
+        ])
+      ])
+    ),
+    u('root', [
+      u('paragraph', [u('text', 'first')]),
+      u('paragraph', [u('text', 'second'), u('text', ' '), u('text', 'value')]),
+      u('list', {ordered: false, start: null, loose: false}, [
+        u('listItem', {loose: false}, []),
+        u('listItem', {loose: false}, [])
+      ])
+    ])
+  )
+  t.end()
+})
