698c6d49c0d002dc5d7862cc05ac44b957708c83
Titus Wormer <tituswormer@gmail.com>
2021-04-23 21:38:07 +0200

    Add JSDoc based types

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
+/**
+ * @typedef {import('unist').Node} Node
+ * @typedef {import('unist-util-remove').TestFunctionAnything} TestFunction
+ */
+
 import {remove} from 'unist-util-remove'
 
+/**
+ * @template {Node} T
+ * @param {T} tree
+ * @returns {T}
+ */
 export function squeezeParagraphs(tree) {
   return remove(tree, {cascade: false}, isEmptyParagraph)
 }
 
-// Whether paragraph is empty or composed only of whitespace.
+/**
+ * Whether paragraph is empty or composed only of whitespace.
+ *
+ * @type {TestFunction}
+ */
 function isEmptyParagraph(node) {
   return (
     node.type === 'paragraph' &&
+    // @ts-ignore paragraphs are parents.
     node.children.every(
-      (node) => node.type === 'text' && /^\s*$/.test(node.value)
+      (/** @type {Node} */ node) =>
+        // @ts-ignore texts are literals.
+        node.type === 'text' && /^\s*$/.test(node.value)
     )
   )
 }
