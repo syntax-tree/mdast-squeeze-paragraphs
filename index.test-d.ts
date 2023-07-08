513ebebec433cfc01a1e2afc93e3b33046754190
Titus Wormer <tituswormer@gmail.com>
2023-07-08 16:51:20 +0200

    Refactor code-style

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,15 +1,13 @@
-import {expectType} from 'tsd'
 import type {Root, Heading, Paragraph} from 'mdast'
+import {expectType} from 'tsd'
 import {squeezeParagraphs} from './index.js'
 
 const root: Root = {type: 'root', children: []}
 const paragraph: Paragraph = {type: 'paragraph', children: []}
 const heading: Heading = {type: 'heading', depth: 1, children: []}
-/* eslint-disable @typescript-eslint/consistent-type-assertions */
 const headingOrParagraph = {type: 'paragraph', children: []} as
   | Heading
   | Paragraph
-/* eslint-enable @typescript-eslint/consistent-type-assertions */
 
 expectType<Root>(squeezeParagraphs(root))
 expectType<Paragraph | null>(squeezeParagraphs(paragraph))
--- test.js
+++ test.js
@@ -1,50 +1,45 @@
-/**
- * @typedef {import('mdast').Root} Root
- * @typedef {import('mdast').Paragraph} Paragraph
- */
-
 import assert from 'node:assert/strict'
 import test from 'node:test'
 import {u} from 'unist-builder'
 import {squeezeParagraphs} from './index.js'
 
-test('squeezeParagraphs', () => {
-  assert.deepEqual(
-    squeezeParagraphs(
-      /** @type {Root} */ (
-        u('root', [
-          u('paragraph', []),
-          u('paragraph', [u('text', 'first')]),
-          u('paragraph', []),
-          u('paragraph', [u('text', ''), u('text', '  \n')]),
-          u('paragraph', [
-            u('text', 'second'),
-            u('text', ' '),
-            u('text', 'value')
-          ]),
-          u('paragraph', []),
-          u('list', {ordered: false, start: null, loose: false}, [
-            u('listItem', {loose: false}, [u('paragraph', [])]),
-            u('listItem', {loose: false}, [
-              u('paragraph', [u('text', ' '), u('text', ' ')])
-            ])
-          ])
-        ])
-      )
-    ),
-    u('root', [
+test('squeezeParagraphs', async function (t) {
+  await t.test('should work on a tree', async function () {
+    const tree = u('root', [
+      u('paragraph', []),
       u('paragraph', [u('text', 'first')]),
+      u('paragraph', []),
+      u('paragraph', [u('text', ''), u('text', '  \n')]),
       u('paragraph', [u('text', 'second'), u('text', ' '), u('text', 'value')]),
+      u('paragraph', []),
       u('list', {ordered: false, start: null, loose: false}, [
-        u('listItem', {loose: false}, []),
-        u('listItem', {loose: false}, [])
+        u('listItem', {loose: false}, [u('paragraph', [])]),
+        u('listItem', {loose: false}, [
+          u('paragraph', [u('text', ' '), u('text', ' ')])
+        ])
       ])
     ])
-  )
 
-  assert.deepEqual(
-    squeezeParagraphs(/** @type {Paragraph} */ (u('paragraph', []))),
-    null,
-    'should return `null` for empty paragraphs'
-  )
+    assert.deepEqual(
+      squeezeParagraphs(tree),
+      u('root', [
+        u('paragraph', [u('text', 'first')]),
+        u('paragraph', [
+          u('text', 'second'),
+          u('text', ' '),
+          u('text', 'value')
+        ]),
+        u('list', {ordered: false, start: null, loose: false}, [
+          u('listItem', {loose: false}, []),
+          u('listItem', {loose: false}, [])
+        ])
+      ])
+    )
+  })
+
+  await t.test('should return `null` for empty paragraphs', async function () {
+    const tree = u('paragraph', [])
+
+    assert.deepEqual(squeezeParagraphs(tree), null)
+  })
 })
